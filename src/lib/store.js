d41b74a08dce65b3ff30deb3a9f21c1eec112d04
GuyARoss <guyallenross@gmail.com>
2019-10-05 16:26:35 -0400

    (feature) store:

--- src/lib/store.js
+++ src/lib/store.js
@@ -11,22 +11,30 @@ class Store {
 
   /**
    * getItem fetches an item from the store
-   * @param {String} id of the object being searched for
-   * @return {Object} object (if found) from the store.
+   * @param {string} user user of the current store.
+   * @param {string} id of the object being searched for
+   * @return {object} object (if found) from the store.
    */
-  getItem(id) {
-    return this.store[id];
+  getItem(user, id) {
+    return this.store[user][id];
   }
 
   /**
    * setItem sets an item to the store
-   * @param {*} id id of the object being set
-   * @param {*} item item assigned to the id in the store
+   * @param {string} user user of the object & id
+   * @param {string} id id of the object being set
+   * @param {object} item item assigned to the id in the store
    */
-  setItem(id, item) {
-    this.store[id] = item;
+  setItem(user, id, item) {
+    this.store[user] = {...this.store[user], [id]: item};
+  }
+
+  /**
+   * resets the store
+   */
+  reset() {
+    this.store= {};
   }
 }
-
 
 export default new Store();
