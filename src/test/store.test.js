86978a4e77e28b409471dee07392a567f8bcfcae
GuyARoss <guyallenross@gmail.com>
2019-10-05 16:27:19 -0400

    added tests for the store.

---
+++ src/test/store.test.js
@@ -0,0 +1,31 @@
+import Store from '../lib/store';
+
+describe('Store', () => {
+  const ids = ['one', 'two', 'three'];
+  const objs = [{toast: 'one'}, {toast: 'f'}, {toast: 'fff'}];
+  const user = 'guy';
+
+  afterEach(() => {
+    Store.reset();
+  });
+
+  it(`should correctly get & set 3 items`, () => {
+    ids.forEach((id, i) => {
+      Store.setItem(user, id, objs[i]);
+    });
+
+    ids.forEach((id, i) => {
+      const item = Store.getItem(user, id);
+      expect(item).toEqual(objs[i]);
+    });
+  });
+
+  it('should be reset if the reset method is called', () => {
+    ids.forEach((id, i) => {
+      Store.setItem(user, id, objs[i]);
+    });
+    Store.reset();
+
+    expect(Store.store).toStrictEqual({});
+  });
+});
