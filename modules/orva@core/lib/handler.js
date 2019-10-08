f07788d90c697d9443c8cad6b114ad66015e3193
GuyARoss <guyallenross@gmail.com>
2019-10-07 22:47:30 -0400

    created core handler.

---
+++ modules/orva@core/lib/handler.js
@@ -0,0 +1,38 @@
+import {generateGRPCClient} from '../../orva@utils';
+
+const PROTO_PATH = __dirname + '/../api/core-guide.proto';
+
+/**
+ * CoreHandler
+ * Resolves/ Interacts with the core orva grpc service.
+ */
+export default class CoreHandler {
+  /**
+   * @param {string} url of the profile service
+   */
+  constructor(url) {
+    this._grpcClient = generateGRPCClient(PROTO_PATH, url, 'grpcCore');
+  }
+
+  /**
+   * processStatement
+   * @param {object} args statement args
+   * @param {string} args.userID userID making the request
+   * @param {string} args.deviceID deviceID making the request
+   * @param {string} args.message requested statement
+   */
+  async processStatement(args) {
+    const r = {
+      UserID: args.userID,
+      DeviceID: args.deviceID,
+      Message: args.Message,
+    };
+
+    return await new Promise((resolve, reject) => {
+      this._grpcClient.ProcessStatement(r, (err, res) => { // eslint-disable-line new-cap
+        if (err) reject(err);
+        resolve(res);
+      });
+    });
+  }
+}
