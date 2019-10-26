f893efd06f5c7be854b00efe286695d57992d5bf
GuyARoss <guyallenross@gmail.com>
2019-10-26 03:52:43 -0400

    added value extraction function.

---
+++ modules/orva@core/lib/helpers/extractValues.js
@@ -0,0 +1,19 @@
+import {createTagged} from '../../internal/pos';
+
+export const extractValues = (req) => {
+  const tagged = createTagged(req.Message);
+
+  const values = [];
+  for (const i in tagged) {
+    if (Object.prototype.hasOwnProperty.call(tagged, i)) {
+      const taggedWord = tagged[i];
+      const tag = taggedWord[1];
+
+      if (tag === 'CD') {
+        values.push(taggedWord[0]);
+      }
+    }
+  }
+
+  return values;
+};
