718ae8879191423acf55bce31f58c31fb51c60a0
GuyARoss <guyallenross@gmail.com>
2019-11-02 00:54:13 -0400

    added cal conf and added bad scores.

--- modules/orva@core/lib/resolvers/unique-resolver.js
+++ modules/orva@core/lib/resolvers/unique-resolver.js
@@ -1,10 +1,16 @@
+import {calcConfidence} from '../../internal/helpers';
+
 export default ({words: exampleWords}, {words: messageWords}) => {
-  let score = 0;
+  const score = [];
   exampleWords.forEach((word, idx) => {
     if (messageWords.includes(word)) {
-      score += 100 >> Math.abs(idx - messageWords.indexOf(word));
+      score.push(100 >> Math.abs(
+          idx - messageWords.indexOf(word)
+      ));
+    } else {
+      score.push(0);
     }
   });
 
-  return score;
+  return calcConfidence(score);
 };
