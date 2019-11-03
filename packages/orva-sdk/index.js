40ccb27317302c4b1649503a5b5f5ea24109bb82
GuyARoss <guyallenross@gmail.com>
2019-11-03 14:25:44 -0500

    restruct core.

---
+++ packages/orva-sdk/index.js
@@ -0,0 +1,9 @@
+export {default as jobHandler} from './src/lib/job-handler';
+export {default as skillHandler} from './src/lib/skill-handler';
+export {default as httpServer} from './src/lib/web-server';
+
+import * as resolvers from './src/lib/resolvers';
+export {resolvers};
+
+import * as helpers from './src/lib/helpers';
+export {helpers};
--- packages/orva-sdk/src/index.js
+++
@@ -1,9 +0,0 @@
-export {default as jobHandler} from './job-handler';
-export {default as skillHandler} from './skill-handler';
-export {default as httpServer} from './web-server';
-
-import * as resolvers from './resolvers';
-export {resolvers};
-
-import * as helpers from './helpers';
-export {helpers};
