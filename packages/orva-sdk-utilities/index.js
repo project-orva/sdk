6ba3a317da9f9d876f51aa35213f9e7ce0112f43
GuyARoss <guyallenross@gmail.com>
2019-11-03 14:34:44 -0500

    cleanup.

--- packages/orva-sdk-utilities/index.js
+++ packages/orva-sdk-utilities/index.js
@@ -1,2 +1,1 @@
-export {default as coreService} from './src/lib/core-service';
-export {default as profileService} from './src/lib/profile-service';
+export {default as store} from './src/lib/store';
---
+++ packages/orva-sdk/src/lib/index.js
@@ -0,0 +1,9 @@
+export {default as jobHandler} from './lib/job-handler';
+export {default as skillHandler} from './lib/skill-handler';
+export {default as httpServer} from './lib/web-server';
+
+import * as resolvers from './lib/resolvers';
+export {resolvers};
+
+import * as helpers from './lib/helpers';
+export {helpers};
