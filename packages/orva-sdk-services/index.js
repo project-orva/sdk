1f0026dd3593c755be72d4d9ee3f99a22762d6a5
GuyARoss <guyallenross@gmail.com>
2019-11-03 14:32:14 -0500

    added babel configs.

--- packages/orva-sdk-services/index.js
+++ packages/orva-sdk-services/index.js
@@ -1,2 +1,2 @@
-export {default as profileService} from '.src/lib/profile-service';
 export {default as coreService} from './src/lib/core-service';
+export {default as profileService} from './src/lib/profile-service';
