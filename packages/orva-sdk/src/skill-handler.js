e02da24c840aa2341e9e3f61bf5452bfc876ee7f
GuyARoss <guyallenross@gmail.com>
2020-05-25 17:47:55 -0400

    refactor core to fit skill service

--- packages/orva-sdk/src/skill-handler.js
+++ packages/orva-sdk/src/skill-handler.js
@@ -1,53 +1,15 @@
 import HTTPServer from './web-server';
-import ConsolidationResolver from './resolvers/consolidation-resolver';
-import UniqueResolver from './resolvers/unique-resolver';
-import SimilarResolver from './resolvers/similar-resolver';
 import GrpcSkillService from './grpc-skill-service';
-
-import { createPOSMapping } from './internal/pos';
-
-import {
-  shrinkMapping,
-  getAverage,
-  selectHighestRankedOperation,
-  scoreResolvers,
-} from './internal/helpers';
-
-const defaultResponse = {
-  Statement: '',
-  AssignedFrom: '',
-  GraphicURL: '',
-  GraphicType: '',
-  Error: '',
-};
-
-const defaultResovlers = [
-  ConsolidationResolver,
-  UniqueResolver,
-  SimilarResolver,
-];
-
-const flattenSkills = (skills) => {
-  const readableSkills = [];
-  for(let i = 0; i < skills.length; i++) {
-    for(let j = 0; j < skills[i].examples.length; j++) {
-      readableSkills.push(skills[i].examples[j]);
-    }
-  }
-
-  return readableSkills;
-}
-
+import uuid from 'uuid';
 export default class SkillHandler {
   constructor({
     server = HTTPServer,
-    resolvers = defaultResovlers,
     skillHostURL,
     skillID,
   }) {
     this.skills = [];
     this.server = server;
-    this.resolvers = resolvers;
+    this.examples = [];
 
     if (skillHostURL === undefined) {
       throw Error("skill host url is required for usage");
@@ -61,81 +23,32 @@ export default class SkillHandler {
   }
 
   async _determineSkill(request, errHandler) {
-    const requestMapping = shrinkMapping(
-      createPOSMapping(request.Message)
-    );
-
-    try {
-      const rankedOperations = await Promise.all(this.skills
-        .map(async ({ examples, handlerCB }) => {
-          // calculate score for each of the examples
-          for (let idx = 0; idx < examples.length; idx++) {
-            const score = await scoreResolvers(this.resolvers, {
-              exampleMapping: examples[idx].posMapping,
-              requestMapping,
-            });
-            scores.push(...score);
-          }
-
-          return {
-            score: getAverage(scores.map((x) => x.sum)),
-            confidence: getAverage(scores.map((x) => x.confidence)),
-            handlerCB,
-          };
-        }));
-
-      // determine which of the examples best fit the input message
-      // then use the handler that belongs to that set of examples
-      const bestOperation = await selectHighestRankedOperation(
-        rankedOperations
-      );
-
-      const handlerResponse = await bestOperation.handlerCB(
-        request,
-        errHandler
-      );
-
-      const { score, confidence } = await bestOperation;
-
-      if (typeof handlerResponse === 'string') {
-        return {
-          ...defaultResponse,
-          Score: score,
-          Statement: handlerResponse,
-          Confidence: confidence,
-        };
-      }
-      return {
-        ...defaultResponse,
-        TypeOf: typeof handlerResponse,
-        ...handlerResponse,
-      };
-    } catch (err) {
-      return {
-        ...defaultResponse,
-        Error: err.message,
-      };
+    if (!!this.skills[request.subsetId]) {
+      this.skills[request.subsetId](request, errHandler);
     }
+    errHandler('subsetId map does not exist');
   }
 
   async _serveSkillInformation() {
     return {
-      skillID: this.skillID,
-      exampleInfo: flattenSkills(this.skills),
+      skillId: this.skillId,
+      examples: this.examples,
     }
   }
 
   async start(port, onStart) {
     const resp = await this.skillClient.registerCurrentInstance({
-      skillID: this.skillID,
-      exampleInfo: flattenSkills(this.skills),
+      skillName: this.skillName,
+      skillID: this.skillId,
+      forwardAddress: this.hostAddress,
+      forwardType: 0, // 0 -> http, no other types currently supported.
     });
     
     if(!resp.IsRegistered) {
-      throw Error('skill cannot be successfully registed.')
+      throw Error('skill cannot be successfully registered')
     }
 
-    onStart(); // ~
+    await onStart(); // ~
 
     this.server(port, async (...args) => {
       const { method } = args[2];
@@ -150,16 +63,13 @@ export default class SkillHandler {
   }
 
   async handleSkill(examples, handlerCB) {
-    const updatedExamples = examples.map((example) => ({
-      exampleText: example,
-      posMapping: shrinkMapping(
-        createPOSMapping(example)
-      ),
-    }));
+    const id = uuid.v4();
 
-    this.skills.push({
-      examples: updatedExamples,
-      handlerCB,
-    });
+    this.examples.concat(examples.map(x => ({
+      GroupID: id,
+      ExampleText: x,
+    })));
+    
+    this.skills[id] = handlerCB;
   }
 }
